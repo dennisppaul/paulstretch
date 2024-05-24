34ab991c53b133677866db77df26c52cf3c669a8
dennisppaul <dennis.paul@hfk-bremen.de>
2024-05-24 09:05:55 +0200

    minor changes

--- examples/cmake/cmake-example.cpp
+++ examples/cmake/cmake-example.cpp
@@ -7,6 +7,6 @@ using namespace std;
 
 int main(int argc, char** argv) {
     PaulStretch stretch(8, 44100 * 0.25, 44100);
-    int mNumRequestedSamples = stretch.get_required_samples();
+    int         mNumRequestedSamples = stretch.get_required_samples();
     cout << "get_required_samples: " << mNumRequestedSamples << endl;
 }
