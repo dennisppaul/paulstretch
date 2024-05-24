8e75ac7fb3609bf898c0eddef8711500a9c41f8c
dennisppaul <dennis.paul@hfk-bremen.de>
2024-05-24 09:05:21 +0200

    added cmake + cleaned up

--- examples/CLI/compile_macOS.sh
+++ examples/CLI/compile_macOS.sh
@@ -1,20 +1,24 @@
+#! /bin/sh
+
 outfile=paulstretch
 rm -f $outfile
+
+PAULSTRETCH_LIB_LOCATION="../.."
 
 # Compile C source files with gcc
 gcc -c \
 -I./contrib \
-../contrib/*.c
+../../contrib/*.c
 
 # Compile C++ source files with g++
 g++ -std=c++14 -DKISSFFT \
 -c \
 -I/opt/homebrew/include \
--I../contrib \
+-I$PAULSTRETCH_LIB_LOCATION/contrib \
 -I./input \
--I../include \
+-I$PAULSTRETCH_LIB_LOCATION/include \
 ./input/*.cpp \
-../src/*.cpp \
+$PAULSTRETCH_LIB_LOCATION/src/*.cpp \
 *.cpp
 
 # Link all object files together with g++
@@ -25,3 +29,7 @@ g++ -std=c++14 \
 
 # Clean up object files if desired
 rm -f *.o
+
+#
+# compile and run with `./compile_macOS.sh ; ./paulstretch INPUT.mp3 OUTPUT.raw`
+# 
